e785030ab2f253de8072e4b1c22480cdab2b5921
Cuevas12 <89288980+Cuevas12@users.noreply.github.com>
2021-09-07 23:19:54 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    contador = 0
+suma = 0
+numero = 0
+while numero >=0:
+ numero= float(input())
+ suma += numero
+ contador += 1
+ if contador != 0:
+     promedio = suma/contador
+     print(promedio)
+ else:
+     print()
     pass
 if __name__=='__main__':
     main()
