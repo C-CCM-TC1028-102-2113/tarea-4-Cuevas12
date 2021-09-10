00daf6157d60068e8400b777a7719091f8730c64
Cuevas12 <89288980+Cuevas12@users.noreply.github.com>
2021-09-09 21:08:18 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,6 @@
-def main():
-    #escribe tu código abajo de esta línea
-    pass
-
-if __name__=='__main__':   
-    main()
+num = int(input())
+for i in range(0,num,2):
+    print("#")
+    i += 1
+    if 1 < num:
+        print("%")
