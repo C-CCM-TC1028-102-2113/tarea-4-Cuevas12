b8aaf8ae32e7033560719577560b58d5b6412307
Christelle <christelle@itesm.mx>
2021-09-17 17:46:11 -0500

    Comentarios en ejercicios promedio y AlternaCaracteres

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,11 +1,12 @@
 def main():
   #escribe tu código abajo de esta línea
-num = int(input())
-for i in range(0,num,2):
-    print("#")
-    i += 1
-    if 1 < num:
-        print("%")
+    num = int(input())
+    for i in range(0,num,2):
+        print("#")
+        i += 1
+        ##Hay que verificar el código, te está imprimiendo un caracter de más.
+        if 1 < num:  
+            print("%")
     pass
 if __name__ == '__main__':
     main()
